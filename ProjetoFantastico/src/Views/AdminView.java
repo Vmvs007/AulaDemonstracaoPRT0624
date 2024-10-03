89c6be633c118ff45c74592f2c92561d76932ebc
Vitor Santos <vmvs007@gmail.com>
2024-10-03 10:31:24 +0100

    Update AdminView.java

--- ProjetoFantastico/src/Views/AdminView.java
+++ ProjetoFantastico/src/Views/AdminView.java
@@ -1,4 +1,10 @@
 package Views;
 
 public class AdminView {
+
+    public void mostrarAlgo1(int num) {
+        System.out.println("Numero: " + num);
+        // 123456789
+        int u = 10;
+    }
 }
