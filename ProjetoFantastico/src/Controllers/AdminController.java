e442027a72b5274a63d64a43e142de6595c90d96
Vitor Santos <vmvs007@gmail.com>
2024-10-03 10:29:43 +0100

    Update AdminController.java

--- ProjetoFantastico/src/Controllers/AdminController.java
+++ ProjetoFantastico/src/Controllers/AdminController.java
@@ -1,4 +1,21 @@
 package Controllers;
 
 public class AdminController {
+
+    public void funcao1(){
+        //12345678
+        int i=0;
+
+        System.out.println("Olá");
+    }
+
+    public void funcao2(){
+        //12345678
+        int x=10;
+        String nome;
+
+        System.out.println("Adeus");
+    }
+
+
 }
